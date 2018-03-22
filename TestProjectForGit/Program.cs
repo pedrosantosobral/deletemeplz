0259884e69c8c2bb40ecd96964f53363e104ac69
Pedro Santos <33328797+pedrosantosobral@users.noreply.github.com>
2018-03-22 15:55:11 +0000

    UPDATE BOYYYYYYYYYYYYYYY

--- TestProjectForGit/Program.cs
+++ TestProjectForGit/Program.cs
@@ -7,6 +7,7 @@ namespace TestProjectForGit
         static void Main(string[] args)
         {
             Console.WriteLine("I'm a test for git");
+            //ja te lixaste boyyyyyyy
         }
     }
 }
